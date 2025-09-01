63984889f679ecbbc3a73c8caa7a00c733a37284
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-01 15:10:23 +0530

    HTTP methods

---
+++ Lecture10.js
@@ -0,0 +1,12 @@
+//! HTTP methods
+
+/*
+? GET -  Serever se kuch mangwana ho 
+? POST - jb kuch bhejna ho ya mutate 
+? PUT - file/photo kuch upload krna 
+? PATCH - kuch update krna hai uske liye 
+? DELETE -  kuch delete krna ho uske liye delete 
+*/
+
+
+//* most commonly GET POST hi use hoti hai 
