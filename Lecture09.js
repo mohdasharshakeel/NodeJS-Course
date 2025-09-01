5f71ae551ea7d475057556c3618baa7d872bb032
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-01 14:57:22 +0530

    node JS

---
+++ Lecture09.js
@@ -0,0 +1,8 @@
+//! what is URL -- agenda 
+//?  https://www.mohdasharshakeel.dev/ 
+
+//* https - hyper text transfer protocol secure 
+//* www.mohdasharshakeel.dev -   ye chic IP address ko point krti hai 
+//* / about /project --- path 
+//* Quairy parameters 
+
