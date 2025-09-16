310956fd9b635e001b1c82b80e4a53c7701b9585
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-16 15:50:33 +0530

    updated

--- PROJECT01/index.js
+++ PROJECT01/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
-const users = require("./MOCK_DATA.json")
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
 
-app.get('/api/users', (req, res) => {
-        return res.json(users)
-})
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+        ${users
+          .map((user) => `<li>${user.first_name} ${user.last_name}</li>`)
+          .join("")}
+    </ul>
+    `;
+  res.send(html);
+});
 
 app.listen(PORT, () => {
-    `server is started on port ${PORT}`
-})
+  console.log(`Server is started on port ${PORT}`);
+});
