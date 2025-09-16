a6ee7ba0a133a24df4a56dc18d1b212e0581ee9b
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-16 15:37:46 +0530

    first task is done - RESTFULL api course

---
+++ PROJECT01/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json")
+
+const app = express();
+const PORT = 8000;
+
+app.get('/users', (req, res) => {
+        return res.json(users)
+})
+
+app.listen(PORT, () => {
+    `server is started on port ${PORT}`
+})
