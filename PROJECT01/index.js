f4fed2e3ca9a62acd2cb92053c61035249167476
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-09-16 15:58:55 +0530

    find by id

--- PROJECT01/index.js
+++ PROJECT01/index.js
@@ -8,16 +8,32 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.get("/users", (req, res) => {
-  const html = `
-    <ul>
-        ${users
-          .map((user) => `<li>${user.first_name} ${user.last_name}</li>`)
-          .join("")}
-    </ul>
-    `;
-  res.send(html);
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  // yahan u ya user, kuch bhi naam chalega — bas andar wahi naam use karo
+  const user = users.find((u) => u.id === id);
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  return res.json(user);
 });
+
+// app.get("/users", (req, res) => {
+//   const html = `
+//     <ul>
+//         ${users
+//           .map((user) => `<li>${user.first_name} ${user.last_name}</li>`)
+//           .join("")}
+//     </ul>
+//     `;
+//   res.send(html);
+// });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is started on port ${PORT}`);
