63cab03e76ce83c2826244df5fc3c57f772585db
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-23 19:25:46 +0530

    updated

---
+++ Chapter07/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const myServer = http.createServer((req, res) => {
+    // console.log('new req rec')
+    console.log(req.headers)
+    res.end('Hello from my server');
+})
+
+myServer.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+})
